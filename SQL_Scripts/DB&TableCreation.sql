dd45d8d16323e65ed297704802ec2118e077cdb8
pritam roy <pritam20002001@gmail.com>
2024-04-18 07:02:42 +0530

    3rd commit

--- SQL_Scripts/DB&TableCreation.sql
+++ SQL_Scripts/DB&TableCreation.sql
@@ -3,3 +3,4 @@ use moviestreaming;
 create table admin(UserName varchar(50) PRIMARY KEY, Password_Hash varchar(50) NOT NULL UNIQUE);
 create table moviedetails(MovieID varchar(10) PRIMARY KEY,MovieName varchar(150) NOT NULL UNIQUE, MovieDescription varchar(300) NOT NULL UNIQUE);
 
+line 1;
