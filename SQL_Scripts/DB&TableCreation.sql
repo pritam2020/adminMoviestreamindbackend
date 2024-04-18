2b92e72bc1c057738b869e7a9a4f45ceb385eb17
pritam roy <pritam20002001@gmail.com>
2024-04-18 07:04:20 +0530

    4th commit

--- SQL_Scripts/DB&TableCreation.sql
+++ SQL_Scripts/DB&TableCreation.sql
@@ -4,3 +4,4 @@ create table admin(UserName varchar(50) PRIMARY KEY, Password_Hash varchar(50) N
 create table moviedetails(MovieID varchar(10) PRIMARY KEY,MovieName varchar(150) NOT NULL UNIQUE, MovieDescription varchar(300) NOT NULL UNIQUE);
 
 line 1;
+line 2;
