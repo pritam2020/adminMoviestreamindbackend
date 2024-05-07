2ec6b87c3d5e2cb35554ea68e74961881b7d5e64
pritam roy <pritam20002001@gmail.com>
2024-05-08 02:09:13 +0530

    add many files like .env also added form to upload movie and it's details

--- SQL_Scripts/DB&TableCreation.sql
+++ SQL_Scripts/DB&TableCreation.sql
@@ -2,3 +2,17 @@ create database moviestreaming;
 use moviestreaming;
 create table admin(UserName varchar(50) PRIMARY KEY, Password_Hash varchar(50) NOT NULL UNIQUE);
 create table moviedetails(MovieID varchar(10) PRIMARY KEY,MovieName varchar(150) NOT NULL UNIQUE, MovieDescription varchar(300) NOT NULL UNIQUE);
+insert into admin values ('admin','admin_pass');
+
+ALTER TABLE moviedetails
+ADD COLUMN Genre ENUM('Comedy', 'Horror', 'Sci-fi', 'Action', 'Drama', 'Romance', 'Fantasy', 'Adventure', 'Biography', 'Documentary', 'Thriller', 'Mystery', 'War', 'Award-winning') NOT NULL,
+add column Language ENUM('English', 'Hindi', 'Bengali') NOT NULL;
+ADD COLUMN OriginalLanguage ENUM('English', 'Hindi', 'Bengali') NOT NULL,
+ADD COLUMN Directors VARCHAR(1000) NOT NULL,
+ADD COLUMN Cast VARCHAR(1000),
+ADD COLUMN ReleaseDate DATE,
+ADD COLUMN IMDBRating FLOAT,
+ADD CONSTRAINT check_IMDBRating CHECK (IMDBRating BETWEEN 0 AND 10);
+
+alter table moviedetails modify column Genre varchar (500) NOT NULL;
+
--- index.js
+++ index.js
@@ -1,66 +1,92 @@
 const express = require("express");
-const fs = require('fs');
-const bodyParser = require('body-parser');
-const mysql = require('mysql2');
-const path = require('path');
-const multer = require('multer');
-const session = require('express-session');
+const fs = require("fs");
+const bodyParser = require("body-parser");
+const mysql = require("mysql2");
+const path = require("path");
+const multer = require("multer");
+const session = require("express-session");
 const app = express();
 const PORT = 3002;
+const dotenv=require('dotenv');
 
 var count = 0;
 
-const storage = multer.diskStorage({
+dotenv.config();
+
+const MovieStorage = multer.diskStorage({
   destination: function (req, file, cb) {
     // Specify the destination directory where the uploaded file should be stored
-    cb(null, './VideoGallery/'); // 'uploads/' is the destination directory in this example
+    cb(null, "./VideoGallery/"); // 'uploads/' is the destination directory in this example
   },
   filename: function (req, file, cb) {
     // Generate a unique filename for the uploaded file
-    cb(null, file.originalname + path.extname(file.originalname));
-  }
+    cb(null, file.originalname);
+  },
 });
+const MovieDestnitation = multer({ storage: MovieStorage });
 
-const destnitation = multer({ storage: storage })
+
+
+
+const ThumbnailStorage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    // Specify the destination directory where the uploaded file should be stored
+    cb(null, "./Thumbnails/"); // 'uploads/' is the destination directory in this example
+  },
+  filename: function (req, file, cb) {
+    // Generate a unique filename for the uploaded file
+    cb(null, file.originalname);
+  },
+});
+const ThumbnailDestnitation = multer({ storage: ThumbnailStorage });
+
+
+
 const connection = mysql.createConnection({
-  host: '192.168.0.104',
-  port: 3306,
-  user: 'adm',
-  password: 'Unique@18',
-  database: 'moviestreaming'
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password:process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 });
 connection.connect((err) => {
   if (err) {
-    console.error('Error connecting to database:', err);
+    console.error("Error connecting to database:", err);
     return;
   }
-  console.log('Connected to database');
+  console.log("Connected to database");
 });
 
 
+
+
 app.set("view engine, ejs");
-app.use(session({
-  secret: 'your_secret_key', // Secret used to sign the session ID cookie
-  resave: false,
-  saveUninitialized: true,
-  // cookie: { maxAge: 60000 }
-}));
 
 
 
-app.use('/protected-route', (req, res, next) => {
+
+app.use(
+  session({
+    secret: "your_secret_key", // Secret used to sign the session ID cookie
+    resave: false,
+    saveUninitialized: true,
+    // cookie: { maxAge: 60000 }
+  }));
+app.use("/protected-route", (req, res, next) => {
   if (!req.session.user) {
     console.log(req.session);
-    console.log("session..."+req.session.user+'\n\n');
-    return res.status(401).end('You must be logged in to access this resource');
+    console.log("session..." + req.session.user + "\n\n");
+    return res.status(401).end("You must be logged in to access this resource");
   }
   console.log(req.session);
-  console.log("session..."+req.session.user.username+'\n\n');
+  console.log("session..." + req.session.user.username + "\n\n");
   next();
-})
-app.use('/protected-route/views', express.static(path.join(__dirname, 'Views', 'console')))
-app.use('/protected-route/videos', express.static(path.join(__dirname, 'VideoGallery')));
-app.use('/protected-route/images', express.static(path.join(__dirname, 'resources')));
+});
+app.use("/protected-route/views",express.static(path.join(__dirname, "Views", "console")));
+app.use("/protected-route/videos",express.static(path.join(__dirname, "VideoGallery")));
+app.use("/protected-route/images",express.static(path.join(__dirname, "resources")));
+
+
 
 
 app.listen(PORT, () => {
@@ -68,70 +94,136 @@ app.listen(PORT, () => {
 });
 
 
-app.get('/', (req, res) => {
-  fs.readFile('./Views/AdminLogin.html', (err, data) => { if (!err) { res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data); } else console.log(err); })
-})
 
 
-
-app.post('/adminlogin', bodyParser.urlencoded({ extended: true }), adminAuthenticate, (req, res) => {
-  console.log('success');
-  //fs.readFile('./Views/console/VideoGallery.html', (err, data) => { if (!err) { res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data); } else console.log(err); });
-  res.redirect('http://192.168.0.104:3002/protected-route/views/VideoGallery.html');
-})
-
-
-
-app.get('/protected-route/videodetails', (req, res) => {
-  connection.query('select * from moviedetails', (err, result) => { if (err) { console.log(err); } else {  console.log("result after fetching movie details..."+result); res.end(JSON.stringify(result)) } })
-})
-
-app.post('/protected-route/Upload', destnitation.any('video'), (req, res) => {
-  console.log("file uploaded...");
-  const description = 'blahh blah blah...' + count;
-  const fileNames = req.files.map((file) => { file.originalname; connection.query(`insert into moviedetails values(${generateMovieID()},'${file.originalname}','${description + count++}')`, (err, result) => { if (!err) { res.sendFile(path.join(__dirname, 'Views', 'console', 'UploadVideo.html')); console.log("movie details uploadede is db..."); console.log(result); } else { console.log(err); } }); });
-  //console.log('Uploaded file names:', fileNames);
-  console.log(req);
-})
+app.get("/", (req, res) => {
+  fs.readFile("./Views/AdminLogin.html", (err, data) => {
+    if (!err) {
+      res.writeHead(200, { "Content-Type": "text/html" });
+      res.end(data);
+    } else console.log(err);
+  });
+});
 
 
 
 
+app.post("/adminlogin",bodyParser.urlencoded({ extended: true }),adminAuthenticate,(req, res) => {
+    console.log("success");
+    //fs.readFile('./Views/console/VideoGallery.html', (err, data) => { if (!err) { res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data); } else console.log(err); });
+    res.redirect(
+      `http://${process.env.SERVER}:${process.env.PORT}/protected-route/views/VideoGallery.html`
+    );
+  }
+);
 
 
-app.get('/protected-route/deletemovie', (req, res) => {
+
+
+app.get("/protected-route/videodetails", (req, res) => {
+  connection.query("select * from moviedetails", (err, result) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("result after fetching movie details..." + result);
+      res.end(JSON.stringify(result));
+    }
+  });
+});
+
+
+
+
+app.post("/protected-route/Upload",bodyParser.urlencoded({ extended: true }),MovieDestnitation.single("video"),ThumbnailDestnitation.single("nail"),(req, res) => {
+    console.log("file uploaded...");
+    // console.log(req)
+    console.log(req.file.filename);
+    console.log(req.file.fieldname);
+    console.log(req.body);
+    const description = "blahh blah blah..." + count;
+    const MovieID=generateMovieID();
+    var genre;var cname;var dname;
+    if(Array.isArray(req.body.genre)){genre = req.body.genre.join();}else {genre=req.body.genre}
+    if(Array.isArray(req.body.dname)){dname = req.body.dname.join();}else {dname=req.body.dname}
+    if(Array.isArray(req.body.cname)){cname = req.body.cname.join();}else {cname=req.body.cname}
+    console.log(genre);
+    console.log(cname);
+    console.log(dname);
+    var SqlQuery="insert into moviedetails values (?,?,?,?,?,?,?,?,?,?)"
+    //  const fileNames = req.files.map((file) => { file.originalname; connection.query(`insert into moviedetails values(${generateMovieID()},'${file.originalname}','${description + count++}')`, (err, result) => { if (!err) { res.sendFile(path.join(__dirname, 'Views', 'console', 'UploadVideo.html')); console.log("movie details uploadede is db..."); console.log(result); } else { console.log(err); } }); });
+    //console.log('Uploaded file names:', fileNames);
+    // console.log(req);
+    connection.query(SqlQuery,
+      [   MovieID,
+          req.file.originalname,
+          req.body.description,
+          genre,
+          req.body.Olanguage,
+          dname,
+          cname,
+          req.body.rdate,
+          req.body.rating,
+          req.body.language],
+      (err, result,fields) => {
+        if (!err) {
+          res.sendFile(
+            path.join(__dirname, "Views", "console", "UploadVideo.html")
+          );
+          console.log("movie details uploadede is db...");
+          console.log(result);
+        } else {
+          console.log(err);
+        }
+      }
+    );
+
+    
+    
+  }
+);
+
+app.get("/protected-route/deletemovie", (req, res) => {
   console.log(req.query.MovieID);
-  const filePath = path.join(__dirname,'VideoGallery',req.query.MovieName+'.mp4');
+  const filePath = path.join(
+    __dirname,
+    "VideoGallery",
+    req.query.MovieName + ".mp4"
+  );
   fs.unlink(filePath, (err) => {
     if (err) {
-      console.error('Error deleting file from storage ...:', err);
+      console.error("Error deleting file from storage ...:", err);
       return;
     }
-    console.log('File deleted successfully from storage...');
-    connection.query(`delete from moviedetails where MovieID=${req.query.MovieID}`, (err, result) => {
-      if (err) {
-        console.log('err in delete  sql...' + err);
-      } else {
-        console.log("No error in delete sql...result: " + result); 
-  
+    console.log("File deleted successfully from storage...");
+    connection.query(
+      `delete from moviedetails where MovieID=${req.query.MovieID}`,
+      (err, result) => {
+        if (err) {
+          console.log("err in delete  sql..." + err);
+        } else {
+          console.log("No error in delete sql...result: " + result);
+        }
       }
-    }
-    )
+    );
     //fs.readFile('./Views/console/VideoGallery.html', (err, data) => { if (!err) { res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data); } else console.log(err); });
-  res.redirect('http://localhost:3002/protected-route/views/videogallery.html');
+    res.redirect(
+      `http://${process.env.SERVER}:${process.env.PORT}/protected-route/views/videogallery.html`
+    );
   });
-})
+});
 
+app.get("/protected-route/logout", (req, res) => {
+  req.session.user.username = "";
 
-
-
-
-
-
-app.get('/protected-route/logout', (req, res) => { 
-  req.session.user.username="";
-  
-  req.session.destroy((err) => { if (err) { console.log(err); res.status(500).send('Error logging out'); } else { console.log('logged out successfully'); res.redirect('http://localhost:3002/') } }) 
+  req.session.destroy((err) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Error logging out");
+    } else {
+      console.log("logged out successfully");
+      res.redirect(`http://${process.env.SERVER}:${process.env.PORT}/`);
+    }
+  });
 });
 
 // app.get('/videogallery/video',(req,res)=>{
@@ -141,8 +233,6 @@ app.get('/protected-route/logout', (req, res) => {
 //   //fs.sendFile('././VideoGallery/one.mp4',(err,data)=>{if(!err){res.writeHead(200,{'Content-Type':'text/html'});}else console.log(err);});
 //   res.sendFile(videopath);
 // })
-
-
 
 function adminAuthenticate(req, res, next) {
   console.log("in adminAuthenticate middleware");
@@ -161,34 +251,40 @@ function adminAuthenticate(req, res, next) {
   //   }
   //   console.log('Connected to database');
   // });
-  connection.query(`SELECT * FROM admin where UserName='${req.body.username}' and Password_Hash='${req.body.password}' `, (err, results) => {
-    if (err) {
-      console.error('Error executing query:', err);
-      return;
-    } else {
-      if (results.length != 0) {
-
+  connection.query(
+    `SELECT * FROM admin where UserName='${req.body.username}' and Password_Hash='${req.body.password}' `,
+    (err, results) => {
+      if (err) {
+        console.error("Error executing query:", err);
+        return;
+      } else {
+        if (results.length != 0) {
           req.session.user = {
-          username: req.body.username,
+            username: req.body.username,
+          };
+          next();
+        } else {
+          console.log("admin User dosenot exists..." + results);
+          fs.readFile("./Views/UserDoseNotExists.html", (err, data) => {
+            if (!err) {
+              res.writeHead(200, { "Content-Type": "text/html" });
+              res.end(data);
+            } else console.log(err);
+          });
         }
-        next();
       }
-      else { console.log("admin User dosenot exists..." + results); fs.readFile('./Views/UserDoseNotExists.html', (err, data) => { if (!err) { res.writeHead(200, { 'Content-Type': 'text/html' }); res.end(data); } else console.log(err); }); }
+      //console.log('Query results:', results);
     }
-    //console.log('Query results:', results);
-
-  });
+  );
 
   // Close the connection when done
   //connection.end();
 }
 
-
-
 function generateMovieID() {
   const length = 10;
-  let result = '';
-  const characters = '0123456789';
+  let result = "";
+  const characters = "0123456789";
   const charactersLength = characters.length;
   for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
@@ -196,3 +292,15 @@ function generateMovieID() {
   return result;
 }
 
+
+async function generatePasswordHash(plaintextPassword){
+  try {
+    // Generate a salt (a random string) to add to the password
+    const saltRounds = 10; // You can adjust this value for more or less security
+    const hashedPassword = await bcrypt.hash(plaintextPassword, saltRounds);
+    return hashedPassword;
+  } catch (error) {
+    console.error('Error hashing password:', error);
+    throw error;
+  }
+}
